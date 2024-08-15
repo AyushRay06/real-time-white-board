60cfd228a0311b290b653b5dccdef2644fc81d89
AyushRay06 <rayayush47@gmail.com>
2024-08-15 07:26:26 +0530

    tooltip

--- app/(dashboard)/_components/sidebar/new-button.tsx
+++ app/(dashboard)/_components/sidebar/new-button.tsx
@@ -4,15 +4,18 @@ import { Plus } from "lucide-react"
 import { CreateOrganization } from "@clerk/nextjs"
 
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog"
+import { Hint } from "@/components/hint"
 
 export const NewButton = () => {
   return (
     <Dialog>
       <DialogTrigger asChild>
         <div className="aspect-square">
-          <button className="bg-white/25 h-full w-full flex items-center justify-center rounded-lg opacity-60 hover:opacity-100">
-            <Plus className="text-white" />
-          </button>
+          <Hint label="Create Organization">
+            <button className="bg-white/25 h-full w-full flex items-center justify-center rounded-lg opacity-60 hover:opacity-100">
+              <Plus className="text-white" />
+            </button>
+          </Hint>
         </div>
       </DialogTrigger>
       <DialogContent className="p-0 border-none bg-transparent max-w-[480px]">
