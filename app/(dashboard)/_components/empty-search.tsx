b5421ab6f302a0c75b723ac17b132ed8185751a5
AyushRay06 <rayayush47@gmail.com>
2024-08-17 09:46:13 +0530

    Button functionality

--- app/(dashboard)/_components/empty-search.tsx
+++ app/(dashboard)/_components/empty-search.tsx
@@ -1,6 +1,12 @@
 "use client"
+
 import { Button } from "@/components/ui/button"
 import Image from "next/image"
+import { useMutation } from "convex/react"
+
+import { api } from "@/convex/_generated/api"
+import { useOrganization } from "@clerk/nextjs"
+import { title } from "process"
 
 interface EmptySearchProps {
   src: string
@@ -10,11 +16,28 @@ interface EmptySearchProps {
 }
 
 export const EmptySearch = ({ src, alt, desc1, desc2 }: EmptySearchProps) => {
+  const { organization } = useOrganization()
+  const create = useMutation(api.board.create)
+
+  const onClick = () => {
+    if (!organization) return
+
+    create({
+      orgId: organization.id,
+      title: "Untitled",
+    })
+  }
+
   return (
     <div className="h-full flex flex-col items-center justify-center">
       <Image src={src} alt={alt} width={300} height={300} />
       <h2 className="text-3xl font-semibold my-4">{desc1}</h2>
       <p className="text-lg text-muted-foreground">{desc2}</p>
+      <div className="pt-6">
+        <Button onClick={onClick} size="lg">
+          Create Organization
+        </Button>
+      </div>
     </div>
   )
 }
