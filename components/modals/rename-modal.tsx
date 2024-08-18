7888691ecf9ead490d27185ad1b8a57c518a9250
AyushRay06 <rayayush47@gmail.com>
2024-08-18 13:06:06 +0530

    rename-modal

--- components/modals/rename-modal.tsx
+++ components/modals/rename-modal.tsx
@@ -1,1 +1,24 @@
 "use client"
+
+import { useRenameModal } from "@/store/use-rename-modal"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "../ui/dialog"
+
+export const RenameModal = () => {
+  const { isOpen, onClose, initialValues } = useRenameModal()
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Edit Board title</DialogTitle>
+        </DialogHeader>
+        <DialogDescription>Enter a new title for the board.</DialogDescription>
+      </DialogContent>
+    </Dialog>
+  )
+}
